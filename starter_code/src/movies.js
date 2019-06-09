5e6aa0cdc1b7b7cb51d11f05a30ae5edc60f00cd
amerzbach <adi.merzbach@gmail.com>
2019-06-09 11:28:47 +0200

    commit 1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,40 @@
 // Get the average of all rates with 2 decimals
 
+function ratesAverage (array) {
+
+    let averageArray = array.map (function(film){ 
+      return parseFloat(film.rate);
+    });
+
+    let ratesAvg = averageArray.reduce(function (a,b){
+      return a + b;
+    });
+
+    return ratesAvg /array.length;
+
+};
+
+console.log(ratesAverage(movies));
+
+/*
+let averageArray = movies.map (function(film){ 
+  return parseFloat(film.rate);
+});
+
+let ratesAverage = averageArray.reduce(function (a,b){
+  return a + b;
+})
+
+ratesAverage = ratesAverage / movies.length;
+
+
+console.log(ratesAverage.toFixed(2));
+
+*/
+
+
+
+
 // Get the average of Drama Movies
 
 // Order by time duration, ascending
