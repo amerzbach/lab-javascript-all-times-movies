75a213ad518747fa34bdaf188a379793569856f5
amerzbach <adi.merzbach@gmail.com>
2019-06-10 11:39:01 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,58 +1,81 @@
 // Get the average of all rates with 2 decimals
 
 
-function ratesAverage (array) {
-  
-  let averageArray = array.map (function(film){ 
-      return parseFloat(film.rate);
-    });
-  
-  let ratesAvg = averageArray.reduce(function (a,b){
-      return a + b;
-  });
+function ratesAverage (movies) {
+  const ratesSum = movies.reduce(function (accumulator,value){
+      let rate = value.rate;
+      if (!rate) rate = 0;      // In case a movie has no rate
+      return accumulator + parseFloat(rate);
+  }, 0);
 
-    return ratesAvg /array.length;
+  // If we do not initialize to 0 the accumulator,
+  // the first value of the accumulator would be the first
+  // object of the array, not a number.
 
+  const average = parseFloat ((ratesSum/movies.length).toFixed(2));
+  return average;
 };
-console.log(Math.floor(ratesAverage(movies)*100)/100);
-
 
 // Get the average of Drama Movies
 
-function dramaMoviesRate (array) {
+function dramaMoviesRate (movies) {
 
-  let dramaMoviesArray = array.slice().filter (function(film) {
+  const dramaMoviesArray = movies.filter (function(film) {
     return (film.genre.includes("Drama"));
   })
 
-  /* Does not work yet
-
-  dramaMoviesArray = dramaMoviesArray.map(function(film) {
-    
-
-    if (film.hasOwnProperty("rate")) return film;
-    else {
-      console.log(film.rate);
-      film.rate = "0";
-      return film;
-    }
-  })
-   */
-
   if (dramaMoviesArray.length === 0) return 0
   else {
-    const n = ratesAverage(dramaMoviesArray);
-    return Math.floor(n*100)/100;
+    const average = ratesAverage(dramaMoviesArray);
+    return average;
   }
 }
 
-console.log(dramaMoviesRate(movies));
-
 // Order by time duration, ascending
+
+function orderByDuration(moviesArray) {
+  
+  const sorted = moviesArray.slice().sort(function (a,b){
+    if (a.duration === b.duration) {
+      return a.title.localeCompare(b.title);
+    }
+    return a.duration - b.duration;
+  })
+  
+  return sorted;
+}
+
+// How many movies did Steven Spielberg direct
+
+function howManyMovies (movies) {
+  let spielbergMovies = movies.filter (function(film) {
+    return ((film.director === "Steven Spielberg")  && film.genre.includes("Drama"));
+  })
+
+  return spielbergMovies.length;
+}
+
+// Order by title and print the first 20 titles
+
+function orderAlphabetically (movies) {
+
+  let orderedFilms = movies.map(function(film) {
+    return film.title;
+  })
+  
+  return orderedFilms.sort(function (a,b){
+    return a.localeCompare(b);
+  }).slice(0,20)
+
+}
+
+
+// Turn duration of the movies from hours to minutes
 
 function getMinutes (string) {
   
-  // This function returns the number of minutes from a string in "_h _m" format
+  // This function returns the number of minutes from a string
+  // in "_h _m" / "_h" or "_m" formats
 
   string = string.replace(" ","");
   let h = "";
@@ -76,102 +99,82 @@ function getMinutes (string) {
   return (minutes);
 }
 
-function orderByDuration(array) {
-  
-  let orderedFilms = array.slice().map(function (film){
-    film.duration = getMinutes(film.duration);
-    return film;
-  })
-  
-  orderedFilms.sort(function (a,b){
-    if (a.duration < b.duration)  return -1;
-    if (a.duration > b.duration)  return 1;
-
-    if (a.title < b.title) return -1;
-    if (a.title > b.title) return 1;
-  })
-
-  return orderedFilms;
-}
-
-orderedMovies = orderByDuration(movies);
-
-/* The Jasmine test does not work but for me is OK, demonstration:
-orderedMovies.forEach(function (film) {
- console.log(film.duration,film.title)
-})
-*/
-
-// How many movies did Steven Spielberg direct
-
-function howManyMovies (array) {
-
-  let spielbergMovies = array.slice().filter (function(film) {
-    return ((film.director === "Steven Spielberg")  && film.genre.includes("Drama"));
-  })
-
-  return spielbergMovies.length;
+function getMinutes(string) {
 
 }
 
-console.log(howManyMovies(movies));
+function turnHoursToMinutes (movies) {
+    const newMovies = movies.map(function (film){
 
-// Order by title and print the first 20 titles
+      // We must to create a new object with object.assign
+      // otherwise we would mutate the object
 
-function orderAlphabetically (array) {
+      const newFilm = Object.assign ({},film);
+      newFilm.duration = getMinutes(film.duration);
+      return newFilm;
 
-  let orderedFilms = array.slice().map(function(film) {
-    return film.title;
-  })
-  
-  orderedFilms.sort(function (a,b){
-    if (a < b) return -1;
-    if (a > b) return 1;
-  })
+      /*
+      The code above is equivalent to
 
-  orderedFilms.splice(20,orderedFilms.length);
-  return orderedFilms;
-}
+      const newFilm = {
+        title:filme.title,
+        year:film.year,
+        director:film.director,
+        genre: film.genre,
+        rate: film.rate,
+        duration: film.duration
+      }
 
-// console.log(orderAlphabetically(movies));
+        newFilm.duration = getMitutes(film.duration);
 
-// Turn duration of the movies from hours to minutes
+      */
 
-function turnHoursToMinutes (array) {
-    let newArray = array.slice().map(function (film){
-      if (film.duration.length > 0) film.duration = getMinutes(film.duration);
-      return film;
     })
-  return newArray;
+  return newMovies;
 }
-
-let updatedArray = turnHoursToMinutes(movies.slice());
 
 // Best yearly rate average
 
-function bestYearAvg (array) {
+function bestYearAvg (movies) {
 
-  if (array.length === 0) return null;
+    //create an array with all singular year values
 
-  let bestYear = [];
+    const years = movies.reduce(function (accumulator,value) {
+      if (!accumulator.includes(value.year)) accumulator.push(value.year);
+      return accumulator;
+    })
 
-  let yearsArray = array.map (function(film){ 
-    return film.year;
-  });
+    // sort the array of years with a comparision function that
+    // compares the average
 
-  console.log(yearsArray);
+    years.sort (function (a,b) {
 
-  uniqueYearsArray = [];
+      const bMovies = movies.filter(function(movie) {
+        return movie.year === b;
+      })
 
-  yearsArray.reduce(uniqueYearsArray,years);
+      const aMovies = movies.filter(function(movie) {
+        return movie.year === a;
+      })
 
-  console.log(uniqueYearsArray);
-  
-  
-  return bestYear;
+      const aAverage = ratesAverages(aMovies);
+      const bAverage = ratesAverages(bMovies);
+
+      if (aAvearge === bAverage) {
+        return Number(a) - Number(b);
+      }
+      return bAverage - aAverage;
+    })
+
+    const bestYear = years[0];
+
+    if (!year) return null;
+
+    const bestYearMovies = movies.filter(function (movie){
+      return movie.year === bestYear;
+    })
+
+    return `The best yeat was ${bestYear} with an average rate of ${ratesAverage(bestYearMovies)`;
+
 }
-
-console.log(bestYearAvg(movies));
-
-
-
+}
